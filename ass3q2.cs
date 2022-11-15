c17dadf93861e9b5efd5456fcb8c05161974af1a
Manasadadi2002 <115692768+Manasadadi2002@users.noreply.github.com>
2022-11-15 12:53:35 +0000

    CsharpAssignment3

---
+++ ass3q2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    internal class Class1
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double DA { get; set; }
+        public double PF { get; set; }
+        public double TDS { get; set; }
+        public double NetSalary { get; set; }
+        public double GrossSalary { get; set; }
+        public double Petrol_allowance { get; set; }
+        public double managersalary { get; set; }
+
+        public int Mar_exec_Kms { get; set; }
+
+
+    }
+}
+ 
