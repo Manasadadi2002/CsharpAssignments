65024f39b552d7c799152e44e25475c5606e11e6
Manasadadi2002 <115692768+Manasadadi2002@users.noreply.github.com>
2022-11-15 12:56:11 +0000

    CsharpAssignment5

---
+++ ass5q2.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ConsoleApp3
+{
+    internal class EmployeeManagementSystem
+    {
+        public static void Main()
+        {
+
+            ArrayList Employee = new ArrayList();
+            Employee.Add(101);
+            Employee.Add("Alekhya");
+            Employee.Add("Trainee");
+            Employee.Add(20000);
+            Employee.Add("Banglore");
+
+
+
+            foreach (object i in Employee)
+            {
+                Console.WriteLine(i + " ");
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
